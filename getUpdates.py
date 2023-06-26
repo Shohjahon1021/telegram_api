00de94b12d0d62d949e953d9791682900e40b90f
Shohjahon1021 <shohjahonberdimurotov21@gmail.com>
2023-06-26 14:29:56 +0500

    get user from loop

--- getUpdates.py
+++ getUpdates.py
@@ -4,7 +4,10 @@ from pprint import pprint
 TOKEN = '5758535081:AAGYdZ2rSo-r0_l-liwWe8CnKHigWSnItjY'
 url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
 response = requests.get(url)
-update = response.json()
+data = response.json()
+updates = data['result']
+for update in updates:
+    messenge = update['message']
+    user = messenge['from']
+    pprint(user)
 
-pprint(update)
-print(response.url)
